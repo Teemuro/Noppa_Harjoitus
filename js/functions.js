beb960c236372d217f1b44e4bb79cdf35b701b7a
Teemuro <n3rete00@students.oamk.fi>
2024-01-30 14:25:54 +0200

    saatu toimimaan

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,17 @@
-const getRandomIntNumberInRange =(min,max) => {
-    return Math.floor(math.random() * max) + min;
-}
-document.querySelector('button').addEventListener('click',() => {
-    const randomized_number = getRandomIntNumberInRange(1,6)
+const dice = document.querySelector("#dice")
+const images = [
+    "./img/1.png",
+    "./img/2.png",
+    "./img/3.png",
+    "./img/4.png",
+    "./img/5.png",
+    "./img/6.png",
+]
+
+dice.addEventListener('click',function() {
+    const randomnumber = Math.floor(Math.random() * images.length)
+    const randomimage = images[randomnumber]
+    const img = document.querySelector("#dice img")
+    img.src =randomimage
 })
+
